4a4d4b6563ac50c9f853f80abc8ae24559ba0286
jesswinw <jesswinw@gmail.com>
2019-09-18 23:39:30 +0530

    Created Basic Scrapping File

---
+++ octoscrap.py
@@ -0,0 +1,37 @@
+import requests
+import os
+from bs4 import BeautifulSoup as bs
+import pprint
+
+try:
+    os.mkdir("Octocats")
+    os.chdir("Octocats")
+
+except OSError:
+    os.chdir("Octocats")
+
+base_url = "https://octodex.github.com/"
+
+
+
+resp = requests.get(base_url)
+soup = bs(resp.text, 'html.parser')
+
+img = soup.find_all("img")
+links = [x.get('data-src') for x in img]
+
+img_source = [base_url + x for x in links if x!=None]
+
+#downloading images
+
+for link in img_source:
+    fname = link.split('/')[-1]
+
+    r = requests.get(link)
+
+    if(os.path.exists(fname)):
+        print(f"{fname} exists")
+    else:
+        print(f"Downloading {fname}")
+        with open(fname,'wb') as f:
+            f.write(r.content)
